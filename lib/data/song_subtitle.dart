e7df884af4af33784944545257514de1c14142f8
Solongo1997 <solongos091@gmail.com>
2022-03-22 09:34:36 +0800

    first commit

---
+++ lib/data/song_subtitle.dart
@@ -0,0 +1,16 @@
+const songTitles = [
+  "song1",
+  "song2",
+  "song3",
+  "song4",
+  "song5",
+  "song6",
+  "song7",
+  "song8",
+  "song9",
+  "song10",
+  "song11",
+  "song12",
+  "song13",
+  "song14",
+];
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../data/song_subtitle.dart';
+import 'lyrics_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My music"),
+      ),
+      body: SongList(),
+    );
+  }
+}
+
+class SongList extends StatelessWidget {
+  const SongList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: songTitles.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            title: Text(songTitles[index]),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              print(songTitles[index]);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LyricsScreen()),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
